ec5dc4c381955277d37a15beea832f0101adb37d
Clément Deseine <clement.deseine@gmail.com>
2018-02-06 12:49:42 +0100

    fix query parameter

--- Epicture/Epicture.BaseAPI/PictureParameters.cs
+++ Epicture/Epicture.BaseAPI/PictureParameters.cs
@@ -2,8 +2,8 @@
 {
     public class PictureParameters
     {
-        public string Name;
-        public string[] Tags { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
         public int MinWidth { get; set; }
         public int MaxWidth { get; set; }
         public int MinHeight { get; set; }
