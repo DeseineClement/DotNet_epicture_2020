ed21c5a4cb53a76131f05591cd69f3299932aad3
Weakky <rav4g3@hotmail.fr>
2018-02-06 18:36:19 +0100

    Started api implementation

---
+++ Epicture/Epicture.FlickrAPI/FlickrAuthenticator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Epicture.BaseAPI;
+
+namespace Epicture.FlickrAPI
+{
+    public class FlickrAuthenticator: AProviderAuthenticator
+    {
+        public FlickrAuthenticator(string clientId, string clientSecret) : base(clientId, clientSecret)
+        {
+        }
+
+        public override async Task<AuthenticationResultBase> Authenticate(string pinCode)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Epicture/Epicture/Views/MainPage.xaml.cs
+++ Epicture/Epicture/Views/MainPage.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Epicture.ImgurAPI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+
+using Epicture.BaseAPI;
 
 namespace Epicture.Views
 {
@@ -12,6 +15,23 @@ namespace Epicture.Views
 		public MainPage()
 		{
 			InitializeComponent();
+
+            this.Appearing += MainPage_Appearing;
 		}
-	}
+
+        private async void MainPage_Appearing(object sender, EventArgs e)
+        {
+            ImgurAuthenticator test = new ImgurAuthenticator("8e0148d95c9866b", "46eba94c9f31b5b053a9f2aa2cb289845a454b7e");
+            AuthenticationResultBase result = await test.Authenticate("71c4dadff2");
+
+            if (result.Success)
+            {
+                AAPIClient client = result.APIClient;
+
+                PicturesResult pics = await client.Search("cats", BaseAPI.Enums.FILE_TYPE.ALL, BaseAPI.Enums.SORT_TYPE.VIRAL);
+                PicturesResult homePics = await client.FetchHomeImages();
+                Console.WriteLine();
+            }
+        }
+    }
 }
